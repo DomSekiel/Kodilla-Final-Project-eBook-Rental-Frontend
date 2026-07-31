54c9c1184b3e8320455197b25d6f08626599df10
Dominik Sekielewski <fran07@gmail.com>
2026-07-31 07:51:32 +0200

    Updated Version

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,6 +1,12 @@
 package base;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.ConfigReader;
--- src/test/java/pages/TitlesPage.java
+++ src/test/java/pages/TitlesPage.java
@@ -1,7 +1,12 @@
 package pages;
 
 import base.BasePage;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.ConfigReader;
